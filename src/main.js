635bfde94380bb0fe75ad293145cb7195ec17181
Rokas <rokas@whatagraph.com>
2019-06-18 16:11:09 +0300

    first

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import FunctionalCalendar from 'vue-functional-calendar';
+Vue.use(FunctionalCalendar, {
+  dayNames: ['Moa', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su']
+});
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,86 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="">
+        <div style="width: 100%; max-width: 350px">
+            <FunctionalCalendar
+                    v-model="calendarData"
+                    :is-date-picker='true'
+                    :disabledDayNames="disabledDays"
+            ></FunctionalCalendar>
+        </div>
+        <div>
+          <div v-for="item in works">
+            <div @click="selectWorkType(item.time)">{{item.name}}</div>
+          </div>
+            <div v-for="item in hours">
+                <div @click="selectHour(item.time)">{{item.timeName}}</div>
+            </div>
+        </div>
+      <div>
+        <p>Type: {{selectedTime}}</p>
+        <p>Time: {{selectedWork}}</p>
+      </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import { FunctionalCalendar } from 'vue-functional-calendar';
+  // @ is an alias to /src
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'home',
+    data () {
+      return {
+        calendarData: {},
+        disabledDays: ['Sa', 'Su','Mo'],
+        selectedTime: '',
+        selectedWork: '',
+        hours: [
+          {
+            timeName: '8',
+            time: '8',
+          },
+          {
+            timeName: '9',
+            time: '9',
+          },
+          {
+            timeName: '10',
+            time: '10',
+          },
+          {
+            timeName: '11',
+            time: '11',
+          },
+          {
+            timeName: '12',
+            time: '12',
+          },
+        ],
+        works: [
+          {
+            name: 'One hour',
+            time: '60'
+          },
+          {
+            name: 'Half hour',
+            time: '30'
+          },
+
+
+        ],
+      }
+    },
+    components: {
+      FunctionalCalendar
+    },
+    methods: {
+      selectHour (time) {
+        console.log(time)
+        this.selectedTime = time
+      },
+      selectWorkType (type) {
+      this.selectedWork = type
+      }
+    }
   }
-}
 </script>
